2b6a83116c4fc91bd63253a8141797a1f79a2d40
chlendyd7 <chlendyd7@naver.com>
2025-02-24 10:53:05 +0900

    feat: recipe serializer config

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -11,7 +11,7 @@ from rest_framework.test import APIClient
 
 from core.models import Recipe
 
-from recipe.serializer import RecipeSerializer
+from recipe.serializers import RecipeSerializer
 
 RECIPES_URL = reverse('recipe:recipe-list')
 
